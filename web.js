cad841e95ee6b266423a48376aa80f02d3f33065
leonardoluiz <leolsc@yahoo.com.br>
2013-07-10 20:35:04 -0300

    Update web.js
    redirecionamento para index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var page = fs.readFile('index.html');
+  response.send(page.toString());
 });
 
 var port = process.env.PORT || 5000;
